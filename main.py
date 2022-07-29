5637a7aca4caa2ff2d78789b867bccaf56ec7c65
KalebeLopes <calbkalebe@gmail.com>
2022-07-29 14:32:50 -0300

    criando endpoint para receber arquivo pickle

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
 from fastapi import FastAPI
 import treinador
 import requests
-import base64
+from pydantic import BaseModel
+# import base64
+
+class Pickle(BaseModel):
+    imagem: str
 
 app = FastAPI()
 
-API_ENDPOINT = "url da api da rasp"
-data = open(encodings.pickle, "rb").read()
-data_bytes = data.encode("ascii")
-base64_bytes = base64.b64encode(data_bytes)
-base64_string = base64_bytes.decode("ascii")
+API_ENDPOINT = "http://127.0.0.1:8000/teste"
 
 @app.get("/")
 def home():
     treinador.iniciarTreinamento()
-    request = requests.post(url = API_ENDPOINT, data = data)
-    response_url = request.text
+    pickle = open("encodings.pickle", "rb").read()
+    data = {"imagem": pickle}
+    requisicao = requests.post(url = API_ENDPOINT, data = data)
+    pastebin_url = requisicao.text
+    print(pastebin_url)
+
     return {"Hello": "World"}
+
+@app.post("/teste")
+async def teste(pickle: Pickle):
+    return {pickle.imagem}
