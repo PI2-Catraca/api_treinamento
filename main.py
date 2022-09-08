7bb46355837c1220afe92d0d994bff4de8e2df0d
KalebeLopes <calbkalebe@gmail.com>
2022-09-08 10:02:54 -0300

    parcial: manipulando arquivo da biometria

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ def default(obj):
 
 @app.get("/")
 async def home(usuario: Usuario):
-    fotos = consultar_db('select * from tb_foto where usuario_cpf = %s', (usuario.cpf,))
+    fotos = consultar_db('select * from tb_foto where usuario_cpf = %s and tipo = %s', (usuario.cpf, 'f',))
     try:
         dir = './dataset/{nomeUsuario}'.format(nomeUsuario = usuario.nome)
         os.mkdir(dir)
@@ -65,15 +65,21 @@ async def home(usuario: Usuario):
         print("Diretório já existente.")
 
     for f in fotos:
-        id, cpf, ft = f
+        idFoto, cpfUsuario, ft, tipo = f
         ft_byte = bytes(ft, 'utf-8')
-        foto = open("./dataset/{nomeUsuario}/{idFoto}.jpg".format(nomeUsuario = usuario.nome, idFoto = id), "wb")
-        foto.write(base64.urlsafe_b64decode(bytes(ft_byte)))
 
+        #verifico se o tipo da foto é igual = b (biometria)
+        if tipo == 'b' :
+            bio = open("./biometria/{nomeUsuario}/{cpfUsuario}.date".format(nomeUsuario = usuario.nome, cpfUsuario = cpfUsuario), "wb")
+            bio.write(base64.urlsafe_b64decode(bytes(ft_byte)))
+
+        foto = open("./dataset/{nomeUsuario}/{idFoto}.jpg".format(nomeUsuario = usuario.nome, idFoto = idFoto), "wb")
+        foto.write(base64.urlsafe_b64decode(bytes(ft_byte)))
 
     await treinador.iniciarTreinamento()
 
-    file_path = r'/home/kalebe/Desktop/treinador'
+    file_path = r'/home/others/Desktop/api_treinamento'
     upload_pickle('encodings.pickle', os.path.join(file_path, 'encodings.pickle'), 'pi2-catraca')
+    upload_pickle('biometria', os.path.join(file_path, 'biometria'), 'pi2-catraca')
 
     return {"Encondings enviados"}
