d7d49b9d418c003b6ecbafe26d8e1db2d970b264
KalebeLopes <calbkalebe@gmail.com>
2022-09-16 05:16:01 -0300

    feat: ajustando rota exclusao usuario

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from pydantic import BaseModel
 import psycopg2
 import base64
 import os 
+import shutil
 from xml.etree.ElementTree import Element, SubElement, ElementTree
 from google.cloud import storage
 
@@ -15,19 +16,30 @@ class Usuario(BaseModel):
     nome: str
     cpf: str
 
-def upload_pickle(blob_name, file_path, bucket_name):
+def upload_file(blob_name, file_path, bucket_name):
     try:
         bucket = storage_client.get_bucket(bucket_name)
         blob = bucket.blob(blob_name)
         blob.upload_from_filename(file_path)
-        print('true')
+        print('enviado')
+        return True
+    except Exception as e:
+        print(e)
+        return False
+
+def delete_file(blob_name, file_path, bucket_name):
+    try:
+        bucket = storage_client.get_bucket(bucket_name)
+        blob = bucket.blob(blob_name)
+        blob.delete()
+        print('deletado')
         return True
     except Exception as e:
         print(e)
         return False
 
 def conecta_db():
-  con = psycopg2.connect(host='localhost', port = '15432', user='postgres', password='password')
+  con = psycopg2.connect(host='localhost', database = 'db_catraca' ,port = '15432', user='postgres', password='password')
   return con
 
 def consultar_db(sql, parametro):
@@ -75,10 +87,55 @@ async def home(usuario: Usuario):
 
     await treinador.iniciarTreinamento()
     
-    file_path = r'C:\\git\\PI2-Catraca\\api_treinamento'
-    file_path_biometria = r'C:\\git\\PI2-Catraca\\api_treinamento\biometria'
+    file_path = r'/home/others/Desktop/api_treinamento'
+    file_path_biometria = r'/home/others/Desktop/api_treinamento/biometria'
 
-    upload_pickle('encodings.pickle', os.path.join(file_path, 'encodings.pickle'), 'pi2-catraca')
-    upload_pickle(('{cpfUsuario}.dat').format(cpfUsuario = cpfUsuario), os.path.join(file_path_biometria, './{cpfUsuario}.dat').format(cpfUsuario = cpfUsuario), 'biometria-pi2')
+    upload_file('encodings.pickle', os.path.join(file_path, 'encodings.pickle'), 'pi2-catraca')
+    upload_file(('{cpfUsuario}.dat').format(cpfUsuario = cpfUsuario), os.path.join(file_path_biometria, './{cpfUsuario}.dat').format(cpfUsuario = cpfUsuario), 'biometria-pi2')
+
+    return {"Encondings enviados"}
+
+@app.get("/excluir")
+async def home(usuario: Usuario):
+    # fotos = consultar_db('select * from tb_foto where usuario_cpf = %s', (usuario.cpf,))
+    try:
+        shutil.rmtree('./dataset/{cpfUsuario}'.format(cpfUsuario = usuario.cpf))
+        # dir = './dataset/{cpfUsuario}'.format(cpfUsuario = usuario.cpf)
+        # os.mkdir(dir)
+    except OSError:
+        print("Não foi possível excluir o diretório.")
+
+    try:
+        os.remove('./biometria/{cpfUsuario}.dat'.format(cpfUsuario = usuario.cpf))
+        # dir = './biometria'
+        # os.mkdir(dir)
+    except OSError:
+        print("Não foi possível excluir o arquivo.")
+
+    # cpfUsuario = ''
+    # for f in fotos:
+    #     idFoto, cpfUsuario, ft, tipo = f
+    #     ft_byte = bytes(ft, 'utf-8')
+        
+    #     #verifico se o tipo da foto é igual = b (biometria)
+    #     if tipo == 'b' :
+    #         bio = open("./biometria/{cpfUsuario}.dat".format(nomeUsuario = usuario.nome, cpfUsuario = cpfUsuario), "wb")
+    #         bio.write(base64.urlsafe_b64decode(bytes(ft_byte)))
+    #         bio.close()
+    #         cpfUsuario = cpfUsuario
+    #         continue
+
+    #     foto = open("./dataset/{nomeUsuario}/{idFoto}.jpg".format(nomeUsuario = usuario.nome, idFoto = idFoto), "wb")
+    #     foto.write(base64.urlsafe_b64decode(bytes(ft_byte)))
+    #     foto.close()
+
+    await treinador.iniciarTreinamento()
+    
+    file_path = r'/home/others/Desktop/api_treinamento'
+    # file_path_biometria = r'/home/others/Desktop/api_treinamento/biometria'
+
+    upload_file('encodings.pickle', os.path.join(file_path, 'encodings.pickle'), 'pi2-catraca')
+    delete_file(usuario.cpf+'.dat', '', 'biometria-pi2')
+    # delete_file(('{cpfUsuario}.dat').format(cpfUsuario = cpfUsuario), os.path.join(file_path_biometria, './{cpfUsuario}.dat').format(cpfUsuario = cpfUsuario), 'biometria-pi2')
 
     return {"Encondings enviados"}
